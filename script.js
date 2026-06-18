01a32ff956e178b7df567acc3ab857ca4a566ae7
sheik muskan <sheikmuskan724@gmail.com>
2026-06-18 14:28:00 +0530

    Advanced Mini CRM

--- script.js
+++ script.js
@@ -1,25 +1,87 @@
-function addLead() {
+let leads=[];
 
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
+function addLead(){
 
-    if(name === "" || email === "" || phone === ""){
-        alert("Please fill all fields");
-        return;
-    }
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+let phone=document.getElementById("phone").value;
+let status=document.getElementById("status").value;
 
-    let li = document.createElement("li");
+if(name==="" || email==="" || phone===""){
+alert("Fill all fields");
+return;
+}
 
-    li.innerHTML =
-    "<b>Name:</b> " + name +
-    "<br><b>Email:</b> " + email +
-    "<br><b>Phone:</b> " + phone +
-    '<br><button onclick="this.parentElement.remove()">Delete</button>';
+let lead={
+name,email,phone,status
+};
 
-    document.getElementById("leadList").appendChild(li);
+leads.push(lead);
 
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("phone").value = "";
+localStorage.setItem("leads",JSON.stringify(leads));
+
+displayLeads();
+}
+
+function displayLeads(){
+
+let leadList=document.getElementById("leadList");
+
+leadList.innerHTML="";
+
+leads.forEach((lead,index)=>{
+
+leadList.innerHTML += `
+<div class="lead">
+<h3>${lead.name}</h3>
+<p>${lead.email}</p>
+<p>${lead.phone}</p>
+<p>Status: ${lead.status}</p>
+
+<button onclick="deleteLead(${index})">
+Delete
+</button>
+</div>
+`;
+
+});
+
+document.getElementById("count").innerText=leads.length;
+}
+
+function deleteLead(index){
+
+leads.splice(index,1);
+
+localStorage.setItem("leads",JSON.stringify(leads));
+
+displayLeads();
+}
+
+function searchLead(){
+
+let search=document.getElementById("search").value.toLowerCase();
+
+let cards=document.querySelectorAll(".lead");
+
+cards.forEach(card=>{
+
+if(card.innerText.toLowerCase().includes(search))
+card.style.display="block";
+else
+card.style.display="none";
+
+});
+
+}
+
+window.onload=function(){
+
+let data=localStorage.getItem("leads");
+
+if(data){
+leads=JSON.parse(data);
+displayLeads();
+}
+
 }
